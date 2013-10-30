2629bc139015bab7d225572499ba3fecf8f8effe
david.bashford <dbashford@bericotechnologies.com>
2013-10-30 14:21:23 -0400

    initial drop

---
+++ src/config.js
@@ -0,0 +1,42 @@
+"use strict";
+
+exports.defaults = function() {
+  return {
+    csslint: {
+      exclude: [],
+      compiled: true,
+      copied: true,
+      vendor: false,
+      rules: {}
+    }
+  };
+};
+
+exports.placeholder = function() {
+  var ph = "  # csslint:                    # settings for javascript hinting\n" +
+     "    # exclude:[]               # array of strings or regexes that match files to not csslint,\n" +
+     "                               # strings are paths that can be relative to the watch.sourceDir\n" +
+     "                               # or absolute\n" +
+     "    # compiled: true           # fire csslint on successful compile of meta-language to javascript\n" +
+     "    # copied: true             # fire csslint for copied javascript files\n" +
+     "    # vendor: false            # fire csslint for copied vendor javascript files (like jquery)\n" +
+     "    # rules:                   # Settings: http://www.csslint.com/options/, these settings will\n" +
+     "                               # override any settings set up in the csslintrc\n" +
+     "      # floats: false          # This is an example override, this is not a default\n\n";
+  return ph;
+};
+
+exports.validate = function (config, validators) {
+  var errors = [];
+
+  if (validators.ifExistsIsObject(errors, "csslint config", config.csslint)) {
+    validators.ifExistsIsObject(errors, "csslint.rules", config.csslint.rules);
+    validators.ifExistsFileExcludeWithRegexAndString(errors, "csslint.exclude", config.csslint, config.watch.sourceDir);
+
+    ['compiled', 'copied', 'vendor'].forEach( function(type) {
+      validators.ifExistsIsBoolean(errors, "csslint." + type, config.csslint[type]);
+    });
+  }
+
+  return errors;
+};
---
+++ src/index.js
@@ -0,0 +1,100 @@
+"use strict";
+
+var csslint = require("csslint").CSSLint,
+    logger = require("logmimosa"),
+    config = require('./config'),
+    lintOptions = {};
+
+var _log = function (fileName, message) {
+  var msg =  "CSSLint Warning: " + message.message + " In " + fileName + ",";
+  if (message.line) {
+    msg += " on line " + message.line + ", column " + message.col + ",";
+  }
+  msg += " from CSSLint rule ID '" + message.rule.id + "'.";
+  logger.warn(msg);
+};
+
+var _lint = function (config, options, next) {
+
+
+  var hasFiles = options.files && options.files.length > 0;
+  if (!hasFiles) {
+    return next();
+  }
+
+  options.files.forEach( function(file, i) {
+    var outputText = file.outputFileText,
+        fileName = file.inputFileName;
+
+    if (outputText && outputText.length > 0) {
+      var doit = true;
+
+      if (config.csslint.exclude && config.csslint.exclude.indexOf(fileName) !== -1) {
+        doit = false;
+      }
+
+      if (config.csslint.excludeRegex && fileName.match(config.csslint.excludeRegex)) {
+        doit = false;
+      }
+
+      if (doit) {
+        if (options.isCopy && !options.isVendor && !config.csslint.copied) {
+          logger.debug("Not linting copied css [[" + fileName + " ]]");
+        } else if (options.isVendor && !config.csslint.vendor) {
+          logger.debug("Not linting vendor css [[ " + fileName + " ]]");
+        } else if (options.isJavascript && !options.isCopy && !config.csslint.compiled) {
+          logger.debug("Not linting compiled css [[ " + fileName + "]]");
+        } else {
+          var result = csslint.verify(outputText, lintOptions);
+          result.messages.forEach( function(message) {
+            _log(fileName, message);
+          });
+        }
+      }
+    }
+
+    if (i === options.files.length-1) {
+      next();
+    }
+  });
+};
+
+var registration = function (config, register) {
+  var extensions = null;
+
+  if (config.csslint.vendor) {
+    logger.debug("vendor being linted, so everything needs to pass through linting");
+    extensions = config.extensions.css;
+  } else if (config.csslint.copied && config.csslint.compiled) {
+    logger.debug("Linting compiled/copied CSS only");
+    extensions = config.extensions.css;
+  } else if (config.csslint.copied) {
+    logger.debug("Linting copied CSS only");
+    extensions = ['css'];
+  } else if (config.csslint.compiled) {
+    logger.debug("Linting compiled CSS only");
+    extensions = config.extensions.css.filter(function (ext) { return ext !== 'css'; } );
+  } else {
+    logger.debug("CSS linting is entirely turned off");
+    extensions = [];
+  }
+
+  if (extensions.length === 0) {
+    return;
+  }
+
+  csslint.getRules().forEach( function(rule) {
+    if (config.csslint.rules[rule.id] !== false) {
+      lintOptions[rule.id] = 1;
+    }
+  });
+
+  register(['add','update','buildExtension','buildFile'], 'afterCompile', _lint, extensions);
+};
+
+module.exports = {
+  registration: registration,
+  defaults: config.defaults,
+  placeholder: config.placeholder,
+  validate: config.validate
+};
